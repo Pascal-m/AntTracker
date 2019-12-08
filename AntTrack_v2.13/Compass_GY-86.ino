1d9f8121bfb29655a011d5f80f3d77419eb2207b
Pascal-m <pascal.moig@gmail.com>
2019-12-08 19:59:06 +0100

    Compas GY86 adatation code
    Adaptation code pour prise en compte compas GY-86

---
+++ AntTrack_v2.13/Compass_GY-86.ino
@@ -0,0 +1,67 @@
+
+#include "Wire.h"
+#include "I2Cdev.h"
+#include "HMC5883L.h"
+
+// Get the mag declination of your location here http://www.magnetic-declination.com/ and insert in # defined in next line
+#define DECLINATION  -0.017 // In degrees
+ 
+ HMC5883L mag;
+  int16_t mx, my, mz;
+//******************************************************************************
+uint8_t Initialise_Compass() {
+  mag.initialize();
+  if(!mag.testConnection()) {
+    Debug.println("No HMC5883L compass found!");
+    return 0;
+  }
+
+  #if defined Debug_All || defined Debug_Compass
+    mag.getHeading(&mx, &my, &mz);
+    Debug.println("----- Compass Found -----");
+    Debug.print  ("mag:\t");
+    Debug.print  (mx); Debug.print("\t");
+    Debug.print  (my); Debug.print("\t");
+    Debug.print  (mz); Debug.print("\t");
+    Debug.println("--------------------------");
+    Debug.println("");
+ #endif
+
+ return 1;
+}
+
+//******************************************************************************
+float GetMagHeading() {
+  
+  // Read Magnetometer
+  mag.getHeading(&mx, &my, &mz);
+  
+  float fHeading = RadToDeg(atan2(my, mx));  //All in degrees now
+  
+  fHeading += DECLINATION;  // Add magnetic declination
+
+  fHeading = Normalise_360(fHeading);
+  
+  #if defined Debug_All || defined Debug_Compass
+    /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
+ //   Debug.print("X: "); Debug.print(mx); Debug.print("  ");
+ //   Debug.print("Y: "); Debug.print(my); Debug.print("  ");
+ //   Debug.print("Z: "); Debug.print(mz); Debug.print("  ");Debug.println("uT");
+    Debug.print("Heading = "); Debug.println(fHeading,0); 
+  #endif   
+
+  return fHeading;
+}
+
+//******************************************************************************
+
+int16_t Normalise_360(int16_t arg) {
+  if (arg < 0) arg += 360;
+  if (arg > 359) arg -= 360;
+  return arg;
+}
+//***************************************************
+float RadToDeg (float _Rad) {
+  return _Rad * 180 / PI;  
+}
+//***************************************************
